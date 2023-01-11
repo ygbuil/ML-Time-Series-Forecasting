c036e66e4b54700183dacc20c4ad3696c999f396
Llorenç Buil <llorenc.buil@pospotential.com>
2023-01-11 17:40:25 +0100

    change class constants to dataclass

--- src/objects/constants.py
+++ src/objects/constants.py
@@ -1,125 +1,122 @@
 # libraries
 import pandas as pd
+from dataclasses import dataclass, field
 
 
+@dataclass
 class Constants:
-    def __init__(
-        self, forecast_group_level, date_column, target_column, use_test_set,
-        period, start_history_date, end_history_date, start_predict_date,
-        end_predict_date, missing_dates, lag_features, list_of_lags,
-        statistical_features, stats_to_calculate, number_of_periods,
-        shift_of_periods, slope_features, slope_past_periods, time_features,
-        time_features_encoding_type, use_cross_validation, cv_n_splits,
-        xgb_hyperparams, parallel_forecast, plot_results
-    ):
-        '''
-        Declares all the constants that will be using during the entire code.
+    '''
+    Declares all the constants that will be using during the entire code.
 
-        Parameters
-        ----------
-        forecast_group_level : list of strings
-            Level at which the forecast will be calculated.
-        date_column : string
-            Date column name.
-        target_column : string
-            Target column name.
-        use_test_set : bool
-            Indicates if test set will be used to check model performance.
-        period: int
-            Periodicity of the Time Series (365 for daily data, 12 for monthly
-            data...).
-        start_history_date : pandas timestamp
-            First day of history used for training.
-        end_history_date : pandas timestamp
-            Last day of history used for training.
-        start_predict_date : pandas timestamp
-            First date to predict.
-        end_predict_date : pandas timestamp
-            Last date to predict.
-        missing_dates : bool
-            Indicates if missing dates have to be added to the Time Series.
-        lag_features : bool
-            Indicates if lag features have to be added to be calculated for
-            training.
-        list_of_lags : list of ints
-            Lags to be added as features.
-        statistical_features : bool
-            Indicates if statistical features have to be added to be calculated
-            for training.
-        stats_to_calculate : list of strings
-            Stats to be calculated as features.
-            Options: ['mean', 'sum', 'std'].
-        number_of_periods : list of ints
-            Periods backwards to calculate stats_to_calculate. E.g.: [7, 30].
-        shift_of_periods : list of ints
-            Shift to apply to number_of_periods. Must be >= 1.
-        slope_features : bool
-            Indicates if slope feature has to be added to be calculated for
-            training.
-        slope_past_periods : int
-            Number of past periods to use to calculate the slope of the linear
-            regression.
-        time_features : bool
-            Indicates if time features have to be added to be calculated for
-            training.
-        time_features_encoding_type : string
-            Type of encoding to apply to time features. Options:
-                - one_hot_encoding: One hot encoding.
-                - sin_cos_encoding: Each value is represented by the projection
-                                    of a vector on the x and y axis.
-                - target_encoding: Expanding mean of target column for each
-                                   time feature.
-        use_cross_validation : bool
-            False: train with default XGBoost hyper parameters.
-            True: cross validate xgb_hyperparams.
-        cv_n_splits : int
-            Number of splits for TimeSeriesSplit cross validation.
-        xgb_hyperparams : dictionary
-            Declares hyperparmeters to try.
-        parallel_forecast : bool
-            True: Forecast each Time Series in parallel.
-            False: Forecast each Time Series sequentialy.
-        plot_results : bool
-            True: Plot forecasted Time Series.
-            False: Do not plot.
+    Parameters
+    ----------
+    forecast_group_level : list of strings
+        Level at which the forecast will be calculated.
+    date_column : string
+        Date column name.
+    target_column : string
+        Target column name.
+    use_test_set : bool
+        Indicates if test set will be used to check model performance.
+    period: int
+        Periodicity of the Time Series (365 for daily data, 12 for monthly
+        data...).
+    start_history_date : pandas timestamp
+        First day of history used for training.
+    end_history_date : pandas timestamp
+        Last day of history used for training.
+    start_predict_date : pandas timestamp
+        First date to predict.
+    end_predict_date : pandas timestamp
+        Last date to predict.
+    missing_dates : bool
+        Indicates if missing dates have to be added to the Time Series.
+    lag_features : bool
+        Indicates if lag features have to be added to be calculated for
+        training.
+    list_of_lags : list of ints
+        Lags to be added as features.
+    statistical_features : bool
+        Indicates if statistical features have to be added to be calculated
+        for training.
+    stats_to_calculate : list of strings
+        Stats to be calculated as features.
+        Options: ['mean', 'sum', 'std'].
+    number_of_periods : list of ints
+        Periods backwards to calculate stats_to_calculate. E.g.: [7, 30].
+    shift_of_periods : list of ints
+        Shift to apply to number_of_periods. Must be >= 1.
+    slope_features : bool
+        Indicates if slope feature has to be added to be calculated for
+        training.
+    slope_past_periods : int
+        Number of past periods to use to calculate the slope of the linear
+        regression.
+    time_features : bool
+        Indicates if time features have to be added to be calculated for
+        training.
+    time_features_encoding_type : string
+        Type of encoding to apply to time features. Options:
+            - one_hot_encoding: One hot encoding.
+            - sin_cos_encoding: Each value is represented by the projection
+                                of a vector on the x and y axis.
+            - target_encoding: Expanding mean of target column for each
+                               time feature.
+    use_cross_validation : bool
+        False: train with default XGBoost hyper parameters.
+        True: cross validate xgb_hyperparams.
+    cv_n_splits : int
+        Number of splits for TimeSeriesSplit cross validation.
+    xgb_hyperparams : dictionary
+        Declares hyperparmeters to try.
+    parallel_forecast : bool
+        True: Forecast each Time Series in parallel.
+        False: Forecast each Time Series sequentialy.
+    plot_results : bool
+        True: Plot forecasted Time Series.
+        False: Do not plot.
 
-        Returns
-        -------
-        None.
+    Returns
+    -------
+    None.
 
-        '''
+    '''
 
-        self.forecast_group_level = forecast_group_level
-        self.date_column = date_column
-        self.target_column = target_column
-        self.use_test_set = use_test_set
-        self.period = period
-        self.start_history_date = start_history_date
-        self.end_history_date = end_history_date
-        self.start_predict_date = start_predict_date
-        self.end_predict_date = end_predict_date
+    forecast_group_level: list
+    date_column: str
+    target_column: str
+    use_test_set: bool
+    period: int
+    start_history_date: pd.Timestamp
+    end_history_date: pd.Timestamp
+    start_predict_date: pd.Timestamp
+    end_predict_date: pd.Timestamp
+    history_dates: pd.DatetimeIndex = field(init=False, default=None)
+    predict_dates: pd.DatetimeIndex = field(init=False, default=None)
+    missing_dates: bool
+    lag_features: bool
+    list_of_lags: list
+    statistical_features: bool
+    stats_to_calculate: list
+    number_of_periods: list
+    shift_of_periods: list
+    slope_features: bool
+    slope_past_periods: int
+    time_features: bool
+    time_features_encoding_type: str
+    use_cross_validation: bool
+    cv_n_splits: int
+    xgb_hyperparams: dict
+    parallel_forecast: bool
+    plot_results: bool
+
+    def __post_init__(self):
         self.history_dates = pd.date_range(
             start=self.start_history_date, end=self.end_history_date, freq='D'
         )
         self.predict_dates = pd.date_range(
             start=self.start_predict_date, end=self.end_predict_date, freq='D'
         )
-        self.missing_dates = missing_dates
-        self.lag_features = lag_features
-        self.list_of_lags = list_of_lags
-        self.statistical_features = statistical_features
-        self.stats_to_calculate = stats_to_calculate
-        self.number_of_periods = number_of_periods
-        self.shift_of_periods = shift_of_periods
-        self.slope_features = slope_features
-        self.slope_past_periods = slope_past_periods
-        self.time_features = time_features
-        self.time_features_encoding_type = time_features_encoding_type
-        self.use_cross_validation = use_cross_validation
-        self.cv_n_splits = cv_n_splits
-        self.xgb_hyperparams = xgb_hyperparams
-        self.parallel_forecast = parallel_forecast
-        self.plot_results = plot_results
 
     def run_checks(self):
         '''
